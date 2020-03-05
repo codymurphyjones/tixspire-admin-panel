f1562c29fed8e7424eadfcad884792f28037c14e
Cody Jones <codymurphyjones@gmail.com>
2020-03-05 14:15:56 -0600

    Uploading image

--- tool/script.js
+++ tool/script.js
@@ -1,25 +1,29 @@
 
-var tixpire = document.getElementById("tixpire");
 var head = document.getElementsByTagName('HEAD')[0];  
 var link = document.createElement('link'); 
 link.rel = 'stylesheet';  
       
 link.type = 'text/css'; 
       
-link.href = 'style.css';  
+link.href = 'https://cdn.jsdelivr.net/gh/codymurphyjones/tixspire-admin-panel@master/tool/style.css';  
 
 head.appendChild(link); 
 
 
-window.onload = function test() {
+function RunTixpire() {
+	var tixpire = document.getElementById("tixpire");
 	tixpire.classList.add("tixpire_button")
 	tixpire.innerHTML = "<button aria-label='Go Now, Pay Later, Tixpire'><div><span>Go Now, Pay Later</span><em></em></div></button>"
 	let urlVal = tixpire.attributes.url.value;
 	console.log(urlVal);
 	tixpire.onclick = () => { console.log("Win!"); window.location.href = urlVal; }
 	tixpire.attributes.removeNamedItem("url");
+	tixpire.attributes.removeNamedItem("id");
 	console.log("loaded");
 }
+
+
+window.onload = RunTixpire;
 /*
 <div class="tixpire_button">
       <button aria-label="Go Now, Pay Later, Tixpire">
