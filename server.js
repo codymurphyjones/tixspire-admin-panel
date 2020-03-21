8303b4172e94671f93fc4b00be9a47a1a979d646
Cody Jones <codymurphyjones@gmail.com>
2020-03-21 16:40:10 -0500

    Server updates

--- server.js
+++ server.js
@@ -5,22 +5,25 @@ const dev = process.env.NODE_ENV !== 'production'
 const port = process.argv[2].replace("$PORT", "3000");;
 const app = next({ dev })
 const handle = app.getRequestHandler()
-
+console.log("Starting");
 app.prepare()
 .then(() => {
   const server = express()
+  console.log("Server init");
   server.options('*', cors()) 
   
-  server.use(express.static('public'))
-  
+  server.use(express.static('public'));
+server.route('/api').all(function (req, res, next) {
+    // runs for all HTTP verbs first
+    // think of it as route specific middleware!
+    res.send('POST request to homepage');
+    return handle(req, res);
+  })
+    
   server.get('*', (req, res) => {
     return handle(req, res)
   })
 
-  server.post('/', function (req, res) {
-    res.send('POST request to homepage')
-  })
-    
   server.listen(port, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
